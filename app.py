c079f2a3b50a5fa4c8b54be50df6800174f76547
VincentBoy <mingsen547896@gmail.com>
2022-10-11 21:31:55 +0800

    Linebot Template

--- app.py
+++ app.py
@@ -9,6 +9,7 @@ from linebot.exceptions import (
 )
 from linebot.models import *
 
+import re 
 
 app = Flask(__name__)
 
@@ -19,7 +20,7 @@ line_bot_api = LineBotApi(
 # 必須放上自己的Channel Secret
 handler = WebhookHandler('188aea8ff2896544e83136e56e2756c4')
 
-line_bot_api.push_message('U066c7cf935fa7a185f301ca749aecc64', TextSendMessage(text='你可以開始了'))
+line_bot_api.push_message('U066c7cf935fa7a185f301ca749aecc64', TextSendMessage(text = '請輸入欲查詢股票資料 格式為 股票 (股名)'))
 
 
 # 監聽所有來自 /callback 的 Post Request
@@ -46,8 +47,45 @@ def callback():
 ##### 基本上程式編輯都在這個function #####
 @handler.add(MessageEvent, message=TextMessage)
 def handle_message(event):
-    message = TextSendMessage(text=event.message.text)
-    line_bot_api.reply_message(event.reply_token,message)
+    #message = TextSendMessage(text=event.message.text)
+    message = text=event.message.text
+    #line_bot_api.reply_message(event.reply_token,message)
+    if "股票 " in message:
+        buttons_template_message = TemplateSendMessage(
+        alt_text = "股票資訊",
+        template = CarouselTemplate(
+            columns = [
+                CarouselColumn(
+                thumbnail_image_url = r"D:\NSYSU_CU\111-1\Fintech\picture\template_pic.png",
+                title = message + "股票資訊",
+                text = "請點選想查詢的股票資訊",
+                actions = [
+                    MessageAction(
+                        label = message[3:] + " 個股資訊",
+                        text= "個股資訊 " + message[3:]
+                    ),
+                    MessageAction(
+                        label= message[3:] + " 個股新聞",
+                        text= "個股新聞 " + message[3:]
+                    )
+                ]
+                )
+            ]
+        )
+    )
+        line_bot_api.reply_message(event.reply_token, buttons_template_message)
+    else:
+        line_bot_api.reply_message(event.reply_token, TextSendMessage("請參照正確格式"))
+
+
+
+# @handler.add(MessageEvent, message=TextMessage) 
+# def handle_message(event):
+# 　　message = event.message.text
+# 　　if re.match("你是誰",message):
+# 　　　　line_bot_api.reply_message(event.reply_token,TextSendMessage("才不告訴你勒~~"))
+# 　　else:
+# 　　　　line_bot_api.reply_message(event.reply_token,TextSendMessage(message))
 
 #主程式
 import os
