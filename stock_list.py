e12f81395fb668193879afabe0e2c8e48c827dae
VincentBoy <mingsen547896@gmail.com>
2023-04-18 22:07:10 +0800

    git commit -m "test"

--- stock_list.py
+++ stock_list.py
@@ -10,3 +10,4 @@ import pyodbc
 connection_string = "Driver=SQL Server;Server=localhost;Database={0};Trusted_Connection=Yes;Database={0};" 
 cnxn = pyodbc.connect(connection_string.format("linebot"), autocommit=True)
 
+## test
